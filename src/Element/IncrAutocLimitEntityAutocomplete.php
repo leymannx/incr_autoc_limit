da25e55eba8e255c2d212406b945ead1edd948e8
Norman Kämper-Leymann <nl@cbe-digital.de>
2016-11-21 09:15:44 +0100

    Set handler

--- src/Element/IncrAutocLimitEntityAutocomplete.php
+++ src/Element/IncrAutocLimitEntityAutocomplete.php
@@ -2,15 +2,7 @@
 
 namespace Drupal\incr_autoc_limit\Element;
 
-use Drupal\Component\Utility\Crypt;
-use Drupal\Component\Utility\Tags;
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface;
-use Drupal\Core\Entity\EntityReferenceSelection\SelectionWithAutocreateInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Entity\Element\EntityAutocomplete;
-use Drupal\Core\Render\Element\Textfield;
-use Drupal\Core\Site\Settings;
 
 /**
  * Provides an entity autocomplete form element.
@@ -26,15 +18,7 @@ class IncrAutocLimitEntityAutocomplete extends EntityAutocomplete {
     $info = parent::getInfo();
     $class = get_class($this);
 
-    $info['#target_type'] = NULL;
-    // I thought changing '#selection_handler' to my ERS plugin would work. It doesn't.
-    // $info['#selection_handler'] = 'incr_autoc_limit_default';
-    $info['#selection_handler'] = 'default';
-    $info['#selection_settings'] = array();
-    $info['#tags'] = FALSE;
-    $info['#autocreate'] = NULL;
-    $info['#validate_reference'] = TRUE;
-    $info['#process_default_value'] = TRUE;
+    $info['#selection_handler'] = 'incr_autoc_limit:node';
 
     $info['#element_validate'] = array(array($class, 'validateEntityAutocomplete'));
     array_unshift($info['#process'], array($class, 'processEntityAutocomplete'));
